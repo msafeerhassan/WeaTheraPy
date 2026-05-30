7e30b4741bed680f8598e33cf0d8bcdee2aa4b5e
msafeerhassan <msafeerhassan0@gmail.com>
2026-05-30 13:36:41 +0500

    enhanced ui

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# ask user about location -> default vs input -> ask user what to see in it -> current temp, max temp, min temp, humidity, wind speed -> option for selecting multiple -> display that
+
 from dotenv import load_dotenv
 import os, requests
 
@@ -20,12 +22,12 @@ def userIPdataAPI():
     else:
         print(f"Failed to fetch data: {ipAPIrespose.status_code}")
 
-def weatherDataAPI(defaultLat, defaultLon):
-    owmURL = f"https://api.openweathermap.org/data/2.5/weather?lat={defaultLat}&lon={defaultLon}&appid={OWMAPI}"
+def weatherDataAPI(Lat, Lon):
+    owmURL = f"https://api.openweathermap.org/data/2.5/weather?lat={Lat}&lon={Lon}&appid={OWMAPI}"
     owmAPIresponse = requests.get(owmURL)
     if owmAPIresponse.status_code == 200:
         owmAPIdata = owmAPIresponse.json()
-        print(owmAPIdata)
+        # print(owmAPIdata)
         cityName = owmAPIdata['name']
         temperature = owmAPIdata['main']['temp'] - 273.15
         minTemp = owmAPIdata['main']['temp_min'] - 273.15
@@ -34,16 +36,23 @@ def weatherDataAPI(defaultLat, defaultLon):
         windSpeed = owmAPIdata['wind']['speed']
         longitude = owmAPIdata['coord']['lon']
         latitude = owmAPIdata['coord']['lat']
-        print(f"""City: {cityName}
-Temperature:{temperature:.1f} °C
-Minimum Temperature Today: {minTemp:.1f} °C
-Maximum Temperature Today: {maxTemp:.1f} °C
-Humidity: {humidity}
-Wind Speed: {windSpeed}""")
-        return latitude, longitude
+        print(f"""--------- WEATHER DASHBOARD ---------
+-------------------------------------
+|City: {cityName}                     |
+-------------------------------------
+|Temperature: {temperature:.1f} °C               |
+-------------------------------------
+|Minimum Temperature Today: {minTemp:.1f} °C |
+-------------------------------------
+|Maximum Temperature Today: {maxTemp:.1f} °C |
+-------------------------------------
+|Humidity: {humidity}                       |   
+-------------------------------------
+|Wind Speed: {windSpeed}                   |
+-------------------------------------""")                 
     else:
         print(f"Can't fetch data from OpenWeatherMap API.\nStatus Code: {owmAPIresponse.status_code}")
 
-# weatherDataAPI(userIPdataAPI()[0], userIPdataAPI()[1])
+weatherDataAPI(userIPdataAPI()[0], userIPdataAPI()[1]) #default location
 
-# userIPdataAPI()
+userIPdataAPI()
